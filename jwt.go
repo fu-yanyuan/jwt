e5d0e134625fa4c95f478ebde392973229919e4f
fu <yanyuan@g.ecc.u-tokyo.ac.jp>
2023-02-24 20:03:35 +0900

    2nd commit

--- jwt.go
+++ jwt.go
@@ -32,7 +32,32 @@ func Hmac() string {
 	return jwtString
 }
 
+func Val(tokenString string) {
+	token, err := jwt.ParseWithClaims(tokenString, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// return our secretkey here
+		return []byte("TestSecretKey"), nil
+	})
+
+	// fmt.Printf("%v \n", err.Error())
+	// fmt.Printf("%v \n", token.Header)
+
+	if claims, ok := token.Claims.(*MyCustomClaims); ok && token.Valid {
+		// do what you want here
+		fmt.Printf("%v", claims.Foo)
+	} else {
+		fmt.Println(err)
+		fmt.Println(token.Valid)
+	}
+}
+
 func main() {
 	ss := Hmac()
 	fmt.Printf("%v \n", ss)
+
+	Val(ss)
 }
